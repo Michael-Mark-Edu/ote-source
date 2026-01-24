2176fa701d6392f9258ebb2a2fe7712cd2672954
Michael Mark <michael.mark@oit.edu>
2026-01-24 04:05:34 -0800

    Deleted repos, Data.Test

--- backend/src/Data.Test/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-global using Xunit;
-global using OTE.Data.EFCore.Contexts;
-global using OTE.Data.EFCore.Dtos;
-global using OTE.Data.EFCore.Entities;
-global using OTE.Data.EFCore.Factories;
-global using OTE.Data.EFCore.Repositories;
