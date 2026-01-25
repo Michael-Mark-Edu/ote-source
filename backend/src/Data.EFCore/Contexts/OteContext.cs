f054e053781f829363cda3399554f9c493871ae9
Michael Mark <michael.mark@oit.edu>
2026-01-24 21:53:58 -0800

    Replaced context factory with singleton

--- backend/src/Data.EFCore/Contexts/OteContext.cs
+++ backend/src/Data.EFCore/Contexts/OteContext.cs
@@ -1,11 +1,13 @@
+using Amazon.RDS.Util;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using OTE.Data.EFCore.Entities;
 
 namespace OTE.Data.EFCore.Contexts;
 
 /// <summary>`DbContext` representing the OTE database schema.</summary>
 /// <param name="options">`DbContextOptions` object passed to the `DbContext` constructor.</param>
-public class OteContext(DbContextOptions<OteContext> options) : DbContext(options)
+public class OteContext : DbContext
 {
     public DbSet<Argon2idPasswordEntity> Argon2idPasswords { get; set; } = null!;
     public DbSet<SchoolEntity> Schools { get; set; } = null!;
@@ -14,5 +16,48 @@ public class OteContext(DbContextOptions<OteContext> options) : DbContext(option
     protected override void OnModelCreating(ModelBuilder builder)
     {
         builder.HasDefaultSchema("public");
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        string? lambdaMode = Environment.GetEnvironmentVariable("OTE_LAMBDA");
+        string connectionString;
+
+        if (lambdaMode == "1")
+        {
+            var password = RDSAuthTokenGenerator.GenerateAuthToken(
+                "ote-db.cvywoma8glcn.us-west-2.rds.amazonaws.com",
+                5432,
+                "api_user");
+
+            connectionString = $"Host=ote-db.cvywoma8glcn.us-west-2.rds.amazonaws.com; Database=otedb; Username=api_user; Password={password}; SSL Mode=Require; Trust Server Certificate=true;";
+        }
+        else
+        {
+            string secretsUuid = "7dd46374-7b55-442e-b4b3-1ae375510d4e";
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddUserSecrets(secretsUuid)
+                .Build();
+
+            connectionString = configuration.GetConnectionString("OteDb")
+                ?? throw new Exception("Could not get connections string");
+        }
+
+        optionsBuilder.UseNpgsql(connectionString);
+    }
+}
+
+public static class OteContextSingleton
+{
+    private static OteContext? _context = null;
+
+    public static OteContext GetOrCreate()
+    {
+        if (_context != null)
+            return _context;
+
+        _context = new OteContext();
+        return _context;
     }
 }
