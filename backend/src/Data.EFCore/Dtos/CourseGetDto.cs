beeef2f539ec904b99f3c3dfa4f0215a28417bf0
dijahkw <khadijah.kw@gmail.com>
2026-04-12 00:03:32 -0700

    completed school get and post dto

---
+++ backend/src/Data.EFCore/Dtos/CourseGetDto.cs
@@ -0,0 +1,63 @@
+using OTE.Data.EFCore.Entities;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json.Serialization;
+
+namespace OTE.Data.EFCore.Dtos;
+
+/// <summary>`IGetDto` for courses in a GET request.</summary>
+public class CourseGetDto : IGetDto<CourseEntity>
+{
+    [JsonPropertyName("courseId")]
+    public int CourseId { get; set; }
+
+    [MaxLength(255)]
+    [JsonPropertyName("courseTitle")]
+    public string CourseTitle { get; set; } = string.Empty;
+
+    [MaxLength(255)]
+    [JsonPropertyName("subject")]
+    public string Subject { get; set; } = string.Empty;
+
+    [MaxLength(4)]
+    [JsonPropertyName("subjectAcronym")]
+    public string SubjectAcronym { get; set; } = string.Empty;
+
+    // Level (100-199, 200-299, 300-399, 400-499)
+    [MaxLength(3)]
+    [JsonPropertyName("level")]
+    public string Level { get; set; } = string.Empty;
+
+    [JsonPropertyName("createdAt")]
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    [JsonPropertyName("deletedAt")]
+    public DateTime? DeletedAt { get; set; } = null;
+
+    [JsonIgnore]
+    [ForeignKey("SchoolId")]
+    public SchoolEntity School { get; set; } = null!;
+
+    [JsonPropertyName("schoolId")]
+    public int SchoolId { get; set; }
+
+    public CourseGetDto(CourseEntity courseEntity)
+    {
+        CourseId = courseEntity.CourseId;
+        CourseTitle = courseEntity.CourseTitle;
+        Subject = courseEntity.Subject;
+        SubjectAcronym = courseEntity.SubjectAcronym;
+        Level = courseEntity.Level;
+        SchoolId = courseEntity.SchoolId;
+    }
+
+    [JsonConstructor]
+    public CourseGetDto(int courseId, string? courseTitle, string? subject, 
+        string? subjectAcronym, string? level, int? schoolId)
+    {
+        CourseId = courseId;
+        CourseTitle = courseTitle;
+
+    }
+}
---
+++ backend/src/Data.EFCore/Dtos/CoursePostDto.cs
@@ -0,0 +1,61 @@
+using OTE.Data.EFCore.Entities;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace OTE.Data.EFCore.Dtos;
+
+/// <summary>Output type for `BookPostDto`.</summary>
+public class CoursePostDtoOutput
+{
+    public required BookEntity BookEntity { get; set; }
+}
+
+/// <summary>`IPostDto` for inserting from a POST request.</summary>
+public class CoursePostDto : IPostDto<CoursePostDtoOutput>
+{
+    [JsonPropertyName("courseId")]
+    public int CourseId { get; set; }
+
+    [MaxLength(255)]
+    [JsonPropertyName("courseTitle")]
+    public string CourseTitle { get; set; } = string.Empty;
+
+    [MaxLength(255)]
+    [JsonPropertyName("subject")]
+    public string Subject { get; set; } = string.Empty;
+
+    [MaxLength(4)]
+    [JsonPropertyName("subjectAcronym")]
+    public string SubjectAcronym { get; set; } = string.Empty;
+
+    // Level (100-199, 200-299, 300-399, 400-499)
+    [MaxLength(3)]
+    [JsonPropertyName("level")]
+    public string Level { get; set; } = string.Empty;
+
+    [JsonIgnore]
+    [ForeignKey("SchoolId")]
+    public SchoolEntity School { get; set; } = null!;
+
+    [JsonPropertyName("schoolId")]
+    public int SchoolId { get; set; }
+
+    public CoursePostDtoOutput Map()
+    {
+        var courseEntity = new CourseEntity
+        {
+            CourseId = 0,
+            CourseTitle = CourseTitle,
+            Subject = Subject,
+            SubjectAcronym = SubjectAcronym,
+            Level = Level,
+
+        };
+
+        return new CoursePostDtoOutput
+        {
+            CourseEntity = courseEntity
+        };
+    }
+}
