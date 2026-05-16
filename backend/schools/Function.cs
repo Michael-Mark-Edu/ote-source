9293e51d792f14732b673c6df0316bc056054f2c
dijahkw <khadijah.kw@gmail.com>
2026-05-15 18:22:01 -0700

    Started on new School API Route

---
+++ backend/schools/Function.cs
@@ -0,0 +1,36 @@
+﻿using Amazon.Lambda.Core;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Annotations;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
+using NSec.Cryptography;
+using OTE.Common.Api;
+using OTE.Data.EFCore.Contexts;
+using OTE.Data.EFCore.Dtos;
+using OTE.Data.EFCore.Entities;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace OTE.Routes.Api.Schools;
+
+public class Function
+{
+    [LambdaFunction]
+    public async Task<APIGatewayHttpApiV2ProxyResponse> FunctionHandler(
+        APIGatewayHttpApiV2ProxyRequest request,
+        ILambdaContext context)
+    {
+        return new APIGatewayHttpApiV2ProxyResponse
+        {
+            StatusCode = 200,
+            Body = JsonSerializer.Serialize(new { message = "School route works" }),
+            Headers = new Dictionary<string, string>
+            {
+                { "Content-Type", "application/json" }
+            }
+        };
+    }
+}
---
+++ backend/src/routes/api/schools/Function.cs
@@ -0,0 +1,21 @@
+﻿using Amazon.Lambda.Core;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Annotations;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
+using NSec.Cryptography;
+using OTE.Common.Api;
+using OTE.Data.EFCore.Contexts;
+using OTE.Data.EFCore.Dtos;
+using OTE.Data.EFCore.Entities;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+
+namespace OTE.Routes.Api.School
+{
+    public class Function
+    {
+
+    }
+}
